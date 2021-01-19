5642a6572c026ccc180e8f1b6a5a052472f1fbd8
Sunpacker <sunpacker@yandex.ru>
2021-01-19 21:04:06 +0400

    Api key hiding

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import config from "./config.json";
 /** Instance of discord client */
 export const bot = new Client();
 
-/** Your discord bot token. TIP: first of all, you need to authorize the bot at https://discord.com/oauth2/authorize?client_id=CLIENT_ID&scope=bot */
+/** Your discord bot token. TIP: first of all, you need to authorize the bot at https://discord.com/oauth2/authorize?client_id=<CLIENT_ID>&scope=bot */
 export const token: string = config.token;
 
 /** Strings to trigger your bot send the commands */
